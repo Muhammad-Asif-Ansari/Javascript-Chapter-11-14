384f9932e2fc9c22ec79157cb8b83eefbe393818
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-09-26 23:25:36 +0500

    Assignment JavaScript
    Assignment JavaScript

---
+++ app.js
@@ -0,0 +1,64 @@
+// ASSIGNMENT 04:
+// Chapter 11-14;
+// QUESTION NO 1:
+//  var num1 = prompt("Enter a first number");
+//  var num2 = prompt("Enter a second number");
+//  if(num1 > num2 ){
+//      document.write(num1 + " is greater than " + num2);
+//  }
+//  else if(num1 < num2 ){
+//      document.write(num2 + " is greater than " + num1);
+//  }
+//  else{
+//     document.write("Dono Equal hai");
+//  }
+
+//  QUESTION NO 2:
+//   var value = prompt("Enter a Number");
+//   if (value > 0){
+//     document.write("This is Positive");
+//   }
+//   else if (value < 0){
+//     document.write("This is Negative");
+//   }
+//   else{
+//     document.write("This is Zero");
+//   }
+
+
+// QUESTION NO 3:
+//  var vowel = prompt("Enter a vowel words".toLowerCase());
+//  if(vowel ===  "a" || vowel === "e"  || vowel === "i" || vowel === "o" || vowel === "u")
+//  {
+//     alert("This is vowel words");
+//  }
+//  else{
+//     alert("This is not vowel words");
+//  }
+
+
+// QUESTION NO 4:
+
+// var rightPassword = "muhammadasif";
+// var userPassword = prompt("Enter Your Password");
+// if(userPassword === "rightPassword"){
+//     alert("Correct! The password you entered matches the original password");
+// }
+// else if(userPassword === ""){
+//     alert( "Please enter your password");
+// }
+// else{
+//     alert("Incorrect password");
+// }
+
+// QUESTION NO 5:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// }
+// else{
+// greeting = "Good evening";
+// }
+
+
